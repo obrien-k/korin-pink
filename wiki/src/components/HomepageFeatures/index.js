0e951efb783cf0f11afa33b1117869c196118c60
Kyle OBrien <obrienk@webbhost.net>
2026-06-15 19:36:05 -0500

    smol fix 2

--- wiki/src/components/HomepageFeatures/index.js
+++ wiki/src/components/HomepageFeatures/index.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'IRC.KORIN.WIKI',
+    title: 'IRC.KORIN.PINK/WIKI',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
